39c6f841093189a4d158ee0f26ed3b681f254684
watanabestudy <93374638+watanabestudy@users.noreply.github.com>
2023-02-01 19:17:00 +0900

    3rd Commit

--- src/GitTest.java
+++ src/GitTest.java
@@ -4,6 +4,21 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("Hello World!!");
+		
+		TestA a = new TestA("AAA");
+		System.out.println(a.getName());
 	}
 
+}
+
+class TestA {
+	String name;
+	
+	TestA(String name){
+		this.name = name;
+	}
+	
+	String getName() {
+		return name;
+	}
 }
